0c4ac8d465324d099f83e0be552262ede34bd1d9
basaryldrm06 <basaryldrm06@gmail.com>
2023-12-31 14:12:05 +0300

    main algorithm is updated

--- binanceAPI/position_utilities.py
+++ binanceAPI/position_utilities.py
@@ -1,11 +1,17 @@
 from account_utilities import get_account_balance, convert_usdt_to_coin, enter_position, place_tp_order, place_sl_order
+import sys
+import os 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from indicators.price import fetch_price
+from config import coin_precision, tp_percentage, sl_percentage, test_mode
+
 def enter_long(client):
     account_balance = get_account_balance(client)
     coin_amount = convert_usdt_to_coin(client, "ETHUSDT", account_balance) * (95 / 100)
     coin_price = fetch_price(client, "ETHUSDT")
 
-    tp_price = float(round(coin_price * (1 + tp), coin_precision))
-    sl_price = float(round(coin_price * (1 - sl), coin_precision))
+    tp_price = float(round(coin_price * (1 + tp_percentage), coin_precision))
+    sl_price = float(round(coin_price * (1 - sl_percentage), coin_precision))
 
     if not test_mode:
         enter_position(client, "LONG", coin_amount, "ETHUSDT")
@@ -19,8 +25,8 @@ def enter_short(client):
     coin_amount = convert_usdt_to_coin(client, "ETHUSDT", account_balance) * (95 / 100)
     coin_price = fetch_price(client, "ETHUSDT")
     
-    tp_price = float(round(coin_price * (1 - tp), coin_precision))
-    sl_price = float(round(coin_price * (1 + sl), coin_precision))
+    tp_price = float(round(coin_price * (1 - tp_percentage), coin_precision))
+    sl_price = float(round(coin_price * (1 + sl_percentage), coin_precision))
 
     if not test_mode:
         enter_position(client, "SHORT", coin_amount, "ETHUSDT")
--- main.py
+++ main.py
@@ -1,13 +1,70 @@
-mera_current_dataset_arr1 = []
-mera_current_dataset_arr2 = []
+from indicators.fetch_all_indicators import fetch_all_indicators
+from time import sleep
+from binanceAPI.position_utilities import enter_long, enter_short
+
+mera_current_dataset_arr = []
+position_types = [None, None]
+tp_prices = [0, 0]
+sl_prices = [0, 0]
+
+current_price = 0
+ema_100 = 0
+macd_12 = 0
+macd_26 = 0
+rsi_6 = 0
+ 
+client = None
+
+def close_position(index):
+    tp_prices[index] = 0
+    sl_prices[index] = 0
+    position_types[index] = None
 
 while True:
-    # fetch indicators
-    if (macd_12 > macd_26) and (macd_12 < 0) and (rsi_6 > 50) and (current_price < ema_100):
-        print("Enter long here")
-    elif (macd_12 < macd_26) and (macd_12 > 0) and (rsi_6 < 50) and (current_price > ema_100):
-        print("Enter short here")
-    elif (macd_12 > macd_26) and (macd_26 > 0) and (rsi_6 > 50) and (current_price > ema_100):
-        print("Enter long here")
-    elif (macd_12 < macd_26) and (macd_26 < 0) and (rsi_6 < 50) and (current_price < ema_100):
-        print("Enter short here")
+    current_price, macd_12, macd_26, rsi_6, ema_100 = fetch_all_indicators(client)
+
+    if position_types[0] == None:
+        if (macd_12 > macd_26) and (macd_12 < 0) and (rsi_6 > 50) and (current_price < ema_100):
+            tp_prices[0], sl_prices[0] = enter_long(client)
+            position_types[0] = "LONG"
+        elif (macd_12 < macd_26) and (macd_12 > 0) and (rsi_6 < 50) and (current_price > ema_100):
+            tp_prices[0], sl_prices[0] = enter_short(client)
+            position_types[0] = "SHORT"
+    if position_types[1] == None:
+        if (macd_12 > macd_26) and (macd_26 > 0) and (rsi_6 > 50) and (current_price > ema_100):
+            tp_prices[1], sl_prices[1] = enter_long(client)
+            position_types[1] = "LONG"
+        elif (macd_12 < macd_26) and (macd_26 < 0) and (rsi_6 < 50) and (current_price < ema_100):
+            tp_prices[1], sl_prices[1] = enter_short(client)
+            position_types[1] = "SHORT"
+
+    if position_types[0] == "LONG":
+        if current_price > tp_prices[0]:
+            save_data()
+            close_position(0)
+        elif current_price < sl_prices[0]:
+            save_data()
+            close_position(0)
+    elif position_types[0] == "SHORT":
+        if current_price < tp_prices[0]:
+            save_data()
+            close_position(0)
+        elif current_price > sl_prices[0]:
+            save_data()
+            close_position(0)
+    if position_types[1] == "LONG":
+        if current_price > tp_prices[1]:
+            save_data()
+            close_position(1)
+        elif current_price < sl_prices[1]:
+            save_data()
+            close_position(1)
+    elif position_types[1] == "SHORT":
+        if current_price < tp_prices[1]:
+            save_data()
+            close_position(1)
+        elif current_price > sl_prices[1]:
+            save_data()
+            close_position(1)
+
+    sleep(5)
