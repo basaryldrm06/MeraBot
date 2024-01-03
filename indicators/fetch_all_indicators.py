77b4635e8709d88836ac921098b38ed42502a1ab
basaryldrm06 <basaryldrm06@gmail.com>
2024-01-03 17:37:35 +0300

    Function multiple arguements are reduced to single IndicatorData object

--- indicators/fetch_all_indicators.py
+++ indicators/fetch_all_indicators.py
@@ -2,15 +2,22 @@ from indicators.rsi import fetch_RSI
 from indicators.ema import fetch_EMA
 from indicators.macd import fetch_MACD
 from indicators.price import fetch_price
+from indicators.bar_prices import get_historical_data
+from data.io_utilities import get_current_date_string
+from data.IndicatorData import IndicatorData
 
 interval = "15m"
 symbol = "ETHUSDT"
 
 def fetch_all_indicators(client):
+    date = get_current_date_string()
+    current_price = fetch_price(client, "ETHUSDT")
     macd_12, macd_26 = fetch_MACD(client, symbol, 12, 26, interval)
     ema_100 = fetch_EMA(client, symbol, 100, interval)
     rsi_6 = fetch_RSI(client, symbol, 6, interval)
-    current_price = fetch_price(client, "ETHUSDT")
+    bar_list = get_historical_data(client)
 
-    return current_price, macd_12, macd_26, ema_100, rsi_6
+    indicatorDataObj = IndicatorData(date, current_price, macd_12, macd_26, ema_100, rsi_6, bar_list)
+
+    return indicatorDataObj
 
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from binance.client import Client
+from binanceAPI.position_utilities import enter_long, enter_short
 from config import api_key, secret_key
 from indicators.fetch_all_indicators import fetch_all_indicators
-from data.io_utilities import get_current_date_string
+from data.io_utilities import get_current_date_string, print_position_message
+from data.IndicatorData import IndicatorData
+from data import io_utilities
+from time import sleep
 
 # BinanceAPI Connection
 client = Client(api_key, secret_key)
@@ -11,6 +15,7 @@ tp_prices = [0, 0]
 sl_prices = [0, 0]
 do_not_enter_long = [0, 0]
 do_not_enter_short = [0, 0]
+position_types = [None, None]
 current_price = 0
 ema_100 = 0
 macd_12 = 0
@@ -37,7 +42,35 @@ elif (macd_12 < macd_26) and (macd_26 < 0):
 
 # Main Algorithm
 while True:
-    current_price, macd_12, macd_26, rsi_6, ema_100 = fetch_all_indicators(client)
+    indicatorDataObj = fetch_all_indicators(client)
     date = get_current_date_string()
+
+    if position_types[0] == None:
+        if (not do_not_enter_long[0]) and (macd_12 > macd_26) and \
+          (macd_12 < 0) and (rsi_6 > 50) and (current_price < ema_100):
+            do_not_enter_short[0] = False
+            do_not_enter_long[0] = True
+            tp_prices[0], sl_prices[0] = enter_long(client)
+            print_position_message(indicatorDataObj, "LONG")
+        elif (not do_not_enter_short[0]) and (macd_12 < macd_26) and \
+          (macd_12 > 0) and (rsi_6 < 50) and (current_price > ema_100):
+            do_not_enter_long[0] = False
+            do_not_enter_short[0] = True
+            tp_prices[0], sl_prices[0] = enter_short(client)
+            print_position_message(indicatorDataObj, "SHORT")
+
+    if position_types[1] == None:
+        if (not do_not_enter_long[1]) and (macd_12 > macd_26) and \
+          (macd_26 > 0) and (rsi_6 > 50) and (current_price > ema_100):
+            do_not_enter_short[1] = False
+            do_not_enter_long[1] = True
+            tp_prices[1], sl_prices[1] = enter_long(client)
+            print_position_message(indicatorDataObj, "LONG")
+        elif (not do_not_enter_short[1]) and (macd_12 < macd_26) and \
+          (macd_26 < 0) and (rsi_6 < 50) and (current_price < ema_100):
+            do_not_enter_long[1] = False
+            do_not_enter_short[1] = True
+            tp_prices[1], sl_prices[1] = enter_short(client)
+            print_position_message(indicatorDataObj, "SHORT")
 
     sleep(10)
