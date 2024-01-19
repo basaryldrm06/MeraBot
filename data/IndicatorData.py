8cc8fe357b69c477abfe2b074c5800a8a50347c6
basaryldrm06 <basaryldrm06@gmail.com>
2024-01-19 21:26:21 +0300

    IO utilities are updated

--- data/IndicatorData.py
+++ data/IndicatorData.py
@@ -1,9 +1,9 @@
 class IndicatorData:
-    def __init__(self, date, price, macd_12, macd_26, rsi_6, ema_100):
+    def __init__(self, date, price, macd_12, macd_26, ema_100, rsi_6, bar_list):
         self.date = date
         self.price = price
         self.macd_12 = macd_12
         self.macd_26 = macd_26
         self.rsi_6 = rsi_6
         self.ema_100 = ema_100
-        self.bar_list = []
+        self.bar_list = bar_list
--- data/io_utilities.py
+++ data/io_utilities.py
@@ -1,16 +1,38 @@
 import datetime
+from termcolor import colored
 
-def print_position_message(indicatorDataObj, position, is_pseudo):
-    print("DATE: " + str(indicatorDataObj.date) + 
-          " PRICE: " + str(indicatorDataObj.price) + 
-          " MACD_12: " + str(indicatorDataObj.macd_12) + 
-          " MACD_26: " + str(indicatorDataObj.macd_26) + 
-          " EMA_100: " + str(indicatorDataObj.ema_100) + 
-          " RSI_6: " + str(indicatorDataObj.rsi_6) + 
-          " Position " + str(position) + 
-          " PSEUDO: " + str(is_pseudo))
+def print_position_message(indicatorDataObj, position):
+    print_with_color("yellow", "PRICE: " + str(round(indicatorDataObj.price, 2)) + 
+          " MACD_12: " + str(round(indicatorDataObj.macd_12, 2)) + 
+          " MACD_26: " + str(round(indicatorDataObj.macd_26, 2)) + 
+          " EMA_100: " + str(round(indicatorDataObj.ema_100, 2)) + 
+          " RSI_6: " + str(round(indicatorDataObj.rsi_6, 2)))
     
 def get_current_date_string(format="%Y-%m-%d %H:%M:%S"):
     current_date = datetime.datetime.now()
     date_string = current_date.strftime(format)
     return date_string
+
+def print_with_color(color, text):
+    colored_text = text
+    if color.lower() == 'cyan':
+        colored_text = colored(text, 'cyan')
+    elif color.lower() == 'green':
+        colored_text = colored(text, 'green')
+    elif color.lower() == 'yellow':
+        colored_text = colored(text, 'yellow')
+    elif color.lower() == 'red':
+        colored_text = colored(text, 'red')
+
+    print("[" + get_current_date_string() + "] " + colored_text)
+
+def calculateWR(tp_count, sl_count):
+    total_trades = tp_count + sl_count
+
+    if total_trades == 0:
+        return "0.00%"
+
+    win_rate = tp_count / total_trades
+    win_rate_percentage = round(win_rate * 100, 2)
+
+    return f"{win_rate_percentage}%"
